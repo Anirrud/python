9d841f534cbe6fc6ee051f1175617ac53a2cfc1b
Anirrud <112750518+Anirrud@users.noreply.github.com>
2022-09-19 10:41:04 +0530

    Create exam_q3.py

---
+++ exam_q3.py
@@ -0,0 +1,21 @@
+m1=75
+m2=87
+m3=78
+total=m1+m2+m3
+print("Total marks obtained =",total)
+avg=total/3
+print("Average marks obtained =",avg)
+if(90<=avg):
+    print("A grade")
+elif(80<=avg<90):
+    print("B grade")
+elif(70<=avg<80):
+    print("C grade")
+elif(60<=avg<70):
+    print("D grade")
+elif(50<=avg<60):
+    print("E grade")
+else:
+    print("F grade")
+
+
