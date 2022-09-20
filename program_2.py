594a5b8108650441c76e1aff12a81fea59ac0636
Anirrud <112750518+Anirrud@users.noreply.github.com>
2022-09-20 10:14:56 +0530

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,8 +1,8 @@
 NumList = []
 Even_Sum = 0
 Odd_Sum = 0
-
 Number = int(input("Please enter the Total Number of List Elements: "))
+
 for i in range(1, Number + 1):
     value = int(input("Please enter the Value of Element : "))
     NumList.append(value)
@@ -11,5 +11,9 @@ for j in range(Number):
         Even_Sum += NumList[j]**2
     else:
         Odd_Sum += NumList[j]**2
-print("\nThe Sum of Even Numbers in this List =  ", Even_Sum)
-print("The Sum of Odd Numbers in this List =  ", Odd_Sum)
+if(Number>=0):
+    print("\nThe Sum of Even Numbers in this List =  ", Even_Sum)
+    print("The Sum of Odd Numbers in this List =  ", Odd_Sum)
+else:
+    print("invalid number")
+    
